0dccfeb21ac8e7fc13f63ae80ff73f50fd8cc8ca
MissDarcy669 <missdunn11@gmail.com>
2021-06-23 23:11:03 +0000

    changes from theia lab environment

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var year = newDate().getFullYear()+parseInt(years);
+    var year = new Date().getFullYear()+parseInt(years);
 
     var amount = principal * years * rate /100;
     document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>";
